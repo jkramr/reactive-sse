319b677dc0af314bae9def09b73f43530e7e79bf
jkramr <yuliya.kramarenko@gmail.com>
2017-03-18 23:52:41 +0200

    Add twitter timeline message source.

--- src/main/java/com/jkramr/demo/ReactiveSseApplication.java
+++ src/main/java/com/jkramr/demo/ReactiveSseApplication.java
@@ -5,69 +5,54 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.http.MediaType;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.Pollers;
 import org.springframework.integration.dsl.channel.MessageChannels;
-import org.springframework.integration.file.dsl.Files;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageHandler;
-import org.springframework.messaging.SubscribableChannel;
+import org.springframework.integration.twitter.inbound.TimelineReceivingMessageSource;
 import org.springframework.social.twitter.api.Twitter;
 import org.springframework.social.twitter.api.impl.TwitterTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.FluxSink;
-
-import java.io.File;
 
 @SpringBootApplication
 @PropertySource("classpath:oauth.properties")
 @RestController
+@IntegrationComponentScan
 public class ReactiveSseApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ReactiveSseApplication.class, args);
   }
 
-  @Bean
-  SubscribableChannel filesChannel() {
-    return MessageChannels.publishSubscribe().get();
-  }
+  private TimelineReceivingMessageSource tweetMessageSource(
+          Twitter twitter
+  ) {
+    TimelineReceivingMessageSource source = new TimelineReceivingMessageSource(
+            twitter, "foo");
 
-  @GetMapping(value = "/files/{name}",
-              produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-  Flux<String> files(@PathVariable String name) {
-    return Flux.create(this::serializeSink);
-  }
 
-  private void serializeSink(FluxSink<String> sink) {
-    FluxSink<String> serialize = sink.serialize();
-
-    MessageHandler handler = message -> serialize.next(formatMessage(message));
-
-    serialize.setCancellation(() -> filesChannel().unsubscribe(handler));
-
-    filesChannel().subscribe(handler);
-  }
-
-  private static String formatMessage(Message<?> msg) {
-    return msg.getPayload().toString();
+    return source;
   }
 
   @Bean
-  IntegrationFlow integrationFlow(
-          @Value("${input-dir:file://${HOME}/Desktop/in}") File in
+  public DirectChannel inputChannel() {
+    return new DirectChannel();
+  }
+
+  @Bean
+  public IntegrationFlow myFlow(
+          Twitter twitter
+
   ) {
     return IntegrationFlows.from(
-            Files.inboundAdapter(in)
-                 .autoCreateDirectory(true),
-            poller -> poller.poller(spec -> spec.fixedRate(1000L))
+            tweetMessageSource(twitter),
+            poller -> poller.poller(Pollers.fixedRate(1000L))
     )
-                           .transform(File.class, File::getAbsolutePath)
-                           .channel(filesChannel())
+                           .channel(this.inputChannel())
+                           .transform(Object::toString)
+                           .channel(MessageChannels.queue())
                            .get();
   }
 
