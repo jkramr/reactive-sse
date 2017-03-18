87522c7435d89fbb5d7aab6388263d9271c9faa6
jkramr <yuliya.kramarenko@gmail.com>
2017-03-18 20:54:14 +0200

    Add twitter integration. Add property resource for twitter integration.

--- src/main/java/com/jkramr/demo/ReactiveSseApplication.java
+++ src/main/java/com/jkramr/demo/ReactiveSseApplication.java
@@ -1,12 +1,28 @@
 package com.jkramr.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.social.twitter.api.Twitter;
+import org.springframework.social.twitter.api.impl.TwitterTemplate;
 
 @SpringBootApplication
+@PropertySource("classpath:oauth.properties")
 public class ReactiveSseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReactiveSseApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ReactiveSseApplication.class, args);
+  }
+
+  @Bean
+  Twitter twitter(
+          @Value("${twitter.oauth.consumerKey}") String consumerKey,
+          @Value("${twitter.oauth.consumerSecret}") String consumerSecret,
+          @Value("${twitter.oauth.accessToken}") String accessToken,
+          @Value("${twitter.oauth.accessTokenSecret}") String accessTokenSecret
+  ) {
+    return new TwitterTemplate(consumerKey, consumerSecret, accessToken, accessTokenSecret);
+  }
 }
