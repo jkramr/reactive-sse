22465d762d5743fd2428e99a7957d889a954a078
jkramr <yuliya.kramarenko@gmail.com>
2017-03-19 00:31:43 +0200

    Add test polling of #trump tweets.

--- src/main/java/com/jkramr/demo/ReactiveSseApplication.java
+++ src/main/java/com/jkramr/demo/ReactiveSseApplication.java
@@ -1,20 +1,29 @@
 package com.jkramr.demo;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.integration.annotation.IntegrationComponentScan;
-import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageSource;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.Pollers;
 import org.springframework.integration.dsl.channel.MessageChannels;
-import org.springframework.integration.twitter.inbound.TimelineReceivingMessageSource;
+import org.springframework.integration.twitter.inbound.SearchReceivingMessageSource;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.SubscribableChannel;
+import org.springframework.social.twitter.api.Tweet;
 import org.springframework.social.twitter.api.Twitter;
 import org.springframework.social.twitter.api.impl.TwitterTemplate;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
 
 @SpringBootApplication
 @PropertySource("classpath:oauth.properties")
@@ -22,23 +31,48 @@ import org.springframework.web.bind.annotation.RestController;
 @IntegrationComponentScan
 public class ReactiveSseApplication {
 
+  public static final long   TWEET_POLL_PERIOD  = 10 * 1000L;
+  public static final String TWEET_SEARCH_QUERY = "#trump";
+
   public static void main(String[] args) {
     SpringApplication.run(ReactiveSseApplication.class, args);
   }
 
-  private TimelineReceivingMessageSource tweetMessageSource(
-          Twitter twitter
+  @Bean
+  CommandLineRunner commandLineRunner() {
+    return args -> {
+      Flux<String> flux = Flux.create(fluxSink -> {
+        FluxSink<String> serialize = fluxSink.serialize();
+
+        MessageHandler messageHandler =
+                message -> {
+                  String messageString = message.getPayload().toString();
+
+                  System.out.println(messageString);
+
+                  serialize.next(messageString);
+                };
+
+        inputChannel().subscribe(messageHandler);
+      });
+    };
+  }
+
+  private MessageSource<?> tweetMessageSource(
+          Twitter twitter,
+          String query
   ) {
-    TimelineReceivingMessageSource source = new TimelineReceivingMessageSource(
+    SearchReceivingMessageSource source = new SearchReceivingMessageSource(
             twitter, "foo");
 
+    source.setQuery(query);
 
     return source;
   }
 
   @Bean
-  public DirectChannel inputChannel() {
-    return new DirectChannel();
+  public SubscribableChannel inputChannel() {
+    return MessageChannels.publishSubscribe().get();
   }
 
   @Bean
@@ -46,13 +80,16 @@ public class ReactiveSseApplication {
           Twitter twitter
 
   ) {
+    AtomicInteger counter = new AtomicInteger();
     return IntegrationFlows.from(
-            tweetMessageSource(twitter),
-            poller -> poller.poller(Pollers.fixedRate(1000L))
+            tweetMessageSource(twitter, TWEET_SEARCH_QUERY),
+            poller -> poller.poller(Pollers.fixedRate(TWEET_POLL_PERIOD))
     )
-                           .channel(this.inputChannel())
-                           .transform(Object::toString)
-                           .channel(MessageChannels.queue())
+                           .transform((Tweet o) -> {
+                             System.out.println(counter.incrementAndGet() + ": " + o.getText());
+                             return o.toString();
+                           })
+                           .channel(inputChannel())
                            .get();
   }
 
